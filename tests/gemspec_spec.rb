73b0974d68411f21dec311472cb608fe66fdd67a
tolomaus <niek.bartholomeus@gmail.com>
2015-08-25 19:38:14 +0200

    Aligned the license specified in the gemspec file with the one used in the github repo

---
+++ tests/gemspec_spec.rb
@@ -0,0 +1,15 @@
+require_relative "spec_helper"
+
+describe 'Gemspec' do
+  before(:each) do
+    cleanup_request_params
+  end
+
+  it 'should have the right license' do
+    config = YAML.load_file(File.expand_path("#{File.dirname(__FILE__)}/../config.yml"))
+
+    expect(config).to have_key("license")
+    expect(config["license"].downcase).not_to include("gpl")
+    expect(config["license"].downcase).not_to include("gnu")
+  end
+end
